f49106f3b452f5165d930b8a3db68af001c00b14
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2018-02-20 23:06:14 +0000

    pseudo codigo

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+/* Ideas para resolver
+--> *opcional, sirve de algo?* Volver el array order con un reduce en un objeto 
+que contenga el id como key. y la cantidad del mismo item como valor de es key
+el uso de ese reduce reviria para no tener que declarar una propiedad por cada kitty distinto
+que fueran contadores que ayudaran a rastrear la especificidad de la orden
+--> funciones para pintar en el DOM, definir si seran metodos de nuestros objetos
+--> Catalogar menu en dos divisiones por precio: 150 y 250
+--> Agregar modal (definir que de ese modal estara estatico en el html 
+  y que se va a pintar a traves del DOM)
+--> en el modal desplegar el listado de items incluyendo tipo de kitty, precio + iva,
+ e imagen
+--> *el metodo para editar la cart que hace? es el momento donde el usuario despues de
+remover un item cierra el modal para disponerse a comprar un item mas, cualquier otro? (slack)
+--> Es cuando aparece el modal el momento en que se permite al usuario
+  quitar y editar la cantidad de kitties ...
+-->  checar carritos de pagina de amazon y waltmart para validar idea anterior 
+--> el metodo para remover items del cart deberia identificar que kitty en especifico se desea remover
+ */
+
 var kitties = [
   {
     id: 'ky-0',
@@ -49,41 +68,63 @@ var kitties = [
   }
 ];
 
-var order = [];
+//var order = [];
 
-function render(array) {
-  const newArray = array.map((kitty) => {
-    return  `<li class="kitty ${kitty.id}">
+
+function render() {
+  for (var i = 0; i < kitties.length; i++) {
+    var kitty = kitties[i];
+    $('.js-gallery-list').append(`
+    <li class="kitty ${kitty.id}">
       <h3 class="title">${kitty.name}</h3>
       <img src="https://www.cryptokitties.co/images/${kitty.thumbnail}"/>
       <p>$${kitty.price}</p>
-    </li>`
-  });
-  $('.js-gallery-list').html(newArray.join(``));
+    </li>
+    `);
+  }
 }
-
-function getId() {
-  var id = $(this).attr('class').split(' ')[1];
-  ordenarKitty(kitties, id);
-}
-
-function ordenarKitty(array, id) {
-  var selectKitty = array.filter(function(item) {
-    return id === item.id;  
-  });
-  order.push(selectKitty[0]);
-  
-}
-
-
 
 function initListeners() {
-  $('.modal').modal();
-  $('.js-gallery-list').on('click', '.kitty', getId);
+  $('.js-gallery-list').on('click', '.kitty', initCart);
   $('.js-btn-cart').on('click', openMenu);
   $('.js-btn-close').on('click', closeMenu);
 }
 
+const Cart = {
+  order: [],
+  sum: function() {
+    let totalSum = 0;
+    for (let i = 0; i < this.order.length; i++) {
+      totalSum += this.order[i].price;
+    }
+    console.log(totalSum);
+    return totalSum;
+  },
+  remove: function() {
+
+  },
+  edit: function() {
+
+  },
+  list: function() {
+    let li = ` `;
+    console.log(this.order)
+    for (let i = 0; i < this.order.length; i++) {
+      li += `${this.order[i].name} ${this.order[i].price} \n`;
+    }
+    console.log(li);
+  }
+}
+
+
+function initCart() {
+  var id = $(this).attr('class').split(' ')[1];
+  for (let i = 0; i < kitties.length; i++) {
+    if(id === kitties[i].id) {
+      Cart.order.push(kitties[i]);
+    }
+  }
+}
 
 function closeMenu() {
   $('.js-btn-cart').removeClass('hidden');
@@ -95,8 +136,10 @@ function openMenu() {
   $('.js-btn-close').removeClass('hidden');
   $('.js-shopping-cart').removeClass('hidden');
   $('.js-btn-cart').addClass('hidden');
+  Cart.sum();
+  Cart.list();
 }
 $(function () {
-  render(kitties);
+  render();
   initListeners();
 });
